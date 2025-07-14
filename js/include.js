f7ccc67a0b3ade858191f4de3cc396b2a9d68525
lovehanyu <duongthihongna@gmail.com>
2025-07-14 20:04:52 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -19,3 +19,13 @@ function includeHTML() {
     }
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.querySelector(".menu-toggle");
+  const menu = document.querySelector("nav .menu");
+
+  if (toggleBtn && menu) {
+    toggleBtn.addEventListener("click", function () {
+      menu.classList.toggle("show");
+    });
+  }
+});
