a091b8c789657e33958750750b29a5a39406d905
lovehanyu <duongthihongna@gmail.com>
2025-07-14 23:43:12 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,4 +1,15 @@
-<script src="js/include.js"></script>
-<script>
-  includeHTML();
-</script>
+function includeHTML() {
+  const z = document.querySelectorAll('[w3-include-html]');
+  z.forEach(elm => {
+    const file = elm.getAttribute("w3-include-html");
+    if (file) {
+      fetch(file)
+        .then(response => response.text())
+        .then(data => {
+          elm.innerHTML = data;
+          elm.removeAttribute("w3-include-html");
+          includeHTML();
+        });
+    }
+  });
+}
