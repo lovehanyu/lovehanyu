32a70d320aa03c588661daecfca33b73f15e6848
lovehanyu <duongthihongna@gmail.com>
2025-07-14 17:10:34 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,1 +1,21 @@
-
+function includeHTML() {
+  const elements = document.querySelectorAll("[w3-include-html]");
+  elements.forEach((el) => {
+    const file = el.getAttribute("w3-include-html");
+    if (file) {
+      fetch(file)
+        .then((res) => {
+          if (res.ok) return res.text();
+          else throw new Error("Không thể tải " + file);
+        })
+        .then((data) => {
+          el.innerHTML = data;
+          el.removeAttribute("w3-include-html");
+          includeHTML(); // Gọi lại nếu có nhúng lồng nhau
+        })
+        .catch((err) => {
+          el.innerHTML = "Lỗi khi tải file: " + err.message;
+        });
+    }
+  });
+}
