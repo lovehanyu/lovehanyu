372a2ae1896871756d931416bf14f674c2b76bda
lovehanyu <duongthihongna@gmail.com>
2025-07-14 17:08:26 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("components/_header.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("header-placeholder").innerHTML = data;
-    });
 
-  fetch("components/_footer.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("footer-placeholder").innerHTML = data;
-  });
