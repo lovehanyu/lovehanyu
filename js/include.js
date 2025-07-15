9d0a3c5c8799a79b3ca074ba4d862e65fd0f3303
lovehanyu <duongthihongna@gmail.com>
2025-07-15 13:33:25 +0800

    Update include.js

--- js/include.js
+++ js/include.js
@@ -6,6 +6,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
       const html = await response.text();
       document.getElementById("header-include").innerHTML = html;
+
+      // Sau khi header được nhúng xong, gắn sự kiện scroll
+      const nav = document.querySelector(".header-bot");
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+          nav.classList.add("scrolled");
+        } else {
+          nav.classList.remove("scrolled");
+        }
+      });
     } catch (err) {
       console.error("Lỗi khi nhúng header:", err);
     }
