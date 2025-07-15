ba7768b1ae87f624ce5d7a3765baa7b3d170c1f4
lovehanyu <duongthihongna@gmail.com>
2025-07-15 15:00:30 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("main-menu");
+
+  menuToggle.addEventListener("click", function () {
+    menu.classList.toggle("show");
+  });
+});
